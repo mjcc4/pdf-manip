3874eb974d4f52ed8acbaff95093cd60e942387f
MJCC <reception.mjcc@laposte.net>
2021-03-11 20:17:26 +0100

    Gestion d'arguments script séparation de pages PDF

--- pdf_extract.py
+++ pdf_extract.py
@@ -1,5 +1,6 @@
 # pdf_extract.py
 
+import sys
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 def split(path, name_of_split):
@@ -13,5 +14,12 @@ def split(path, name_of_split):
             pdf_writer.write(output_pdf)
 
 if __name__ == '__main__':
-    path = 'input.pdf'
-    split(path, 'output')
+	if (len(sys.argv) <= 1):
+		print ('Pas assez d\'arguments fournis')
+		print ('Veuillez fournir le nom du fichier pdf a diviser. Exemple :')
+		print ('py pdf_extract.py pdftosplit.pdf')
+	else:
+		fichier_a_diviser = sys.argv[1]
+		print ('Séparation des pages de ', fichier_a_diviser)
+		split(fichier_a_diviser, 'output')
+		print ('Les fichiers ont étés générés et s\'appellent output1.pdf, output2.pdf, ...')
